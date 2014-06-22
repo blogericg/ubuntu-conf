2c175fe66516f0dce3d9eb2b29691b24448d9e14
konstruktoid <konstruktoid@users.noreply.github.com>
2014-06-22 16:36:30 +0200

    Syslog tester
    Simple syslog tester

---
+++ syslog_tester.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+facility=(kern user mail daemon auth syslog authpriv)
+severity=(emerg alert crit err warn notice info debug)
+n=10
+host=`hostname -f`
+
+for ((i=1; i<=n; i++))
+do
+PRIORITY0="${facility[RANDOM%${#facility[@]}]}" {,}
+PRIORITY1="${severity[RANDOM%${#severity[@]}]}" {,}
+logger -p $PRIORITY0.$PRIORITY1 "$host Syslog Test Message $i/$n"
+done
