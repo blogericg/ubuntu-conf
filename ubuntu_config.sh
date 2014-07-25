c6ff83e63189912ae81c7c2cbef27542e1ca6696
konstruktoid <konstruktoid@users.noreply.github.com>
2014-07-25 22:18:35 +0200

    Fix kernel mod blacklisting

--- ubuntu_config.sh
+++ ubuntu_config.sh
@@ -62,31 +62,38 @@ if ! [[ "$INPUT" == "$RINPUT" ]];
 		echo "Let it begin."
 fi
 
-echo "[X] Installing firewall."
+$i = "0"
+
+echo "[$i] Installing firewall."
 $SUDO bash -c "curl -3 -s $FW_CONF > /etc/init/firewall.conf"
 $SUDO bash -c "curl -3 -s $FW_POLICY > /etc/init.d/firewall"
 $SUDO update-rc.d firewall defaults 2>/dev/null 
 $SUDO sed -i "s/ADMIN=\"127.0.0.1\"/ADMIN=\"$FW_ADMIN\"/" /etc/init.d/firewall
 $SUDO chmod u+x /etc/init.d/firewall
 $SUDO bash -c "/etc/init.d/firewall"
+((i++))
 
-echo "[X] /etc/fstab"
+echo "[$i] /etc/fstab"
 $SUDO bash -c "echo tmpfs /tmp tmpfs defaults,nosuid,nodev,mode=1777,size=100M 0 0 >> /etc/fstab"
 $SUDO bash -c "echo /tmp /var/tmp tmpfs defaults,nosuid,nodev,bind,mode=1777,size=100M 0 0 >> /etc/fstab"
 $SUDO sed -i '/floppy/d' /etc/fstab
 $SUDO mount -a
+((i++))
 
-echo "[X] Updating the package index files from their sources."
+echo "[$i] Updating the package index files from their sources."
 $SUDO $APT update
+((i++))
 
-echo "[X] Upgrading installed packages."
+echo "[$i] Upgrading installed packages."
 $SUDO $APT upgrade 
+((i++))
 
-echo "[X] /etc/hosts.*"
+echo "[$i] /etc/hosts.*"
 $SUDO bash -c "echo sshd : ALL : ALLOW$'\n'ALL: LOCAL, 127.0.0.1 > /etc/hosts.allow"
 $SUDO bash -c "echo ALL: PARANOID > /etc/hosts.deny"
+((i++))
 
-echo "[X] /etc/login.defs"
+echo "[$i] /etc/login.defs"
 $SUDO sed -i 's/^LOG_OK_LOGINS.*/LOG_OK_LOGINS\t\tyes/' /etc/login.defs
 $SUDO sed -i 's/^UMASK.*/UMASK\t\t077/' /etc/login.defs
 $SUDO sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS\t\t1/' /etc/login.defs
@@ -94,26 +101,31 @@ $SUDO sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS\t\t30/' /etc/login.defs
 $SUDO sed -i 's/DEFAULT_HOME.*/DEFAULT_HOME no/' /etc/login.defs
 $SUDO sed -i 's/USERGROUPS_ENAB.*/USERGROUPS_ENAB no/' /etc/login.defs
 $SUDO sed -i 's/^# SHA_CRYPT_MAX_ROUNDS.*/SHA_CRYPT_MAX_ROUNDS\t\t10000/' /etc/login.defs
+((i++))
 
-echo "[X] /etc/sysctl.conf"
+echo "[$i] /etc/sysctl.conf"
 $SUDO bash -c "curl -3 -s $SYSCTL_CONF > /etc/sysctl.conf"
 $SUDO service procps start
+((i++))
 
-echo "[X] /etc/security/limits.conf"
+echo "[$i] /etc/security/limits.conf"
 $SUDO sed -i 's/^# End of file*//' /etc/security/limits.conf
 $SUDO bash -c "echo * hard maxlogins 10 >> /etc/security/limits.conf"
 $SUDO bash -c "echo * hard core 0$'\n'* soft nproc 100$'\n'* hard nproc 150$'\n\n'# End of file >> /etc/security/limits.conf"
+((i++))
 
-echo "[X] Adduser / Useradd" 
+echo "[$i] Adduser / Useradd" 
 $SUDO sed -i 's/DSHELL=.*/DSHELL=\/bin\/false/' /etc/adduser.conf 
 $SUDO sed -i 's/SHELL=.*/SHELL=\/bin\/false/' /etc/default/useradd
 $SUDO sed -i 's/^# INACTIVE=.*/INACTIVE=35' /etc/default/useradd
+((i++))
 
-echo "[X] Root access"
+echo "[$i] Root access"
 $SUDO sed -i 's/^#+ : root : 127.0.0.1/+ : root : 127.0.0.1/' /etc/security/access.conf
 $SUDO bash -c "echo console > /etc/securetty"
+((i++))
 
-echo "[X] Installing base packages."
+echo "[$i] Installing base packages."
 if [[ `$SUDO dmidecode -q --type system | grep -i vmware` ]]; 
 	then
 		VM="open-vm-tools"
@@ -121,9 +133,10 @@ fi
 
 $SUDO debconf-set-selections <<< "postfix postfix/mailname string `hostname -f`"
 $SUDO debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-$SUDO $APT install aide auditd libpam-tmpdir libpam-cracklib apparmor-profiles ntp openssh-server postfix haveged $VM
 
-echo "[X] /etc/ssh/sshd_config"
+$SUDO $APT install aide apparmor-profiles auditd haveged libpam-cracklib libpam-tmpdir ntp openssh-server postfix $VM
+
+echo "[$i] /etc/ssh/sshd_config"
 $SUDO bash -c "echo $'\n'## Groups allowed to connect$'\n'AllowGroups $SSH_GRPS >> /etc/ssh/sshd_config"
 $SUDO sed -i 's/^LoginGraceTime 120/LoginGraceTime 20/' /etc/ssh/sshd_config
 $SUDO sed -i 's/^PermitRootLogin without-password/PermitRootLogin no/' /etc/ssh/sshd_config
@@ -132,30 +145,31 @@ $SUDO bash -c "echo ClientAliveCountMax 0 >> /etc/ssh/sshd_config"
 $SUDO bash -c "echo PermitUserEnvironment no >> /etc/ssh/sshd_config"
 $SUDO bash -c "echo Ciphers aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,3des-cbc,aes192-cbc,aes256-cbc >> /etc/ssh/sshd_config"
 $SUDO /etc/init.d/ssh restart
+((i++))
 
-echo "[X] Passwords and authentication"
+echo "[$i] Passwords and authentication"
 $SUDO sed -i 's/^password[\t].*.pam_cracklib.*/password\trequired\t\t\tpam_cracklib.so retry=3 maxrepeat=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 difok=4/' /etc/pam.d/common-password
 $SUDO sed -i 's/try_first_pass sha512.*/try_first_pass sha512 remember=24/' /etc/pam.d/common-password
 $SUDO sed -i 's/nullok_secure//' /etc/pam.d/common-auth
+((i++))
 
-echo "[X] Cron and at"
+echo "[$i] Cron and at"
 $SUDO bash -c "echo root > /etc/cron.allow"
 $SUDO bash -c "echo root > /etc/at.allow"
+((i++))
 
-echo "[X] Ctrl-alt-delete"
+echo "[$i] Ctrl-alt-delete"
 $SUDO sed -i 's/^exec.*/exec \/usr\/bin\/logger -p security.info \"Ctrl-Alt-Delete pressed\"/' /etc/init/control-alt-delete.conf
+((i++))
 
-echo "[X] Blacklisting kernel modules"
-$SUDO bash -c "echo >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist dccp >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist sctp >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist rds >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist tipc >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist net-pf-31 >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist bluetooth >> /etc/modprobe.d/blacklist.conf"
-$SUDO bash -c "echo blacklist usb-storage >> /etc/modprobe.d/blacklist.conf"
+echo "[$i] Blacklisting kernel modules"
+for mod in dccp sctp rds tipc net-pf-31 bluetooth usb-storage;
+do 
+	$SUDO bash -c "echo install $mod /bin/false >> /etc/modprobe.d/blacklist.conf"
+done
+((i++))
 
-echo "[X] Remove suid bits"
+echo "[$i] Remove suid bits"
 for p in /bin/fusermount /bin/mount /bin/ping /bin/ping6 /bin/su /bin/umount /usr/bin/bsd-write /usr/bin/chage /usr/bin/chfn /usr/bin/chsh /usr/bin/mlocate /usr/bin/mtr /usr/bin/newgrp /usr/bin/pkexec /usr/bin/traceroute6.iputils /usr/bin/wall /usr/sbin/pppd;
 do 
 	oct=`stat -c "%a" $p |sed 's/^4/0/'`
@@ -164,11 +178,13 @@ do
 	$SUDO dpkg-statoverride --add $ug $oct $p 2> /dev/null
 	$SUDO chmod -s $p
 done
+((i++))
 
-echo "[X] Cleaning."
+echo "[$i] Cleaning."
 $SUDO $APT clean
 $SUDO $APT autoclean
 $SUDO apt-get -qq autoremove
+((i++))
 
 echo
 
