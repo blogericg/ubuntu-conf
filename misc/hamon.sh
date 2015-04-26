ea87d50d8d8fd20383eee998ffe32952948c637e
konstruktoid <konstruktoid@users.noreply.github.com>
2015-04-26 21:19:00 +0200

    retab

--- misc/hamon.sh
+++ misc/hamon.sh
@@ -4,22 +4,22 @@ FQDN=`hostname --fqdn`
 RCPT="root"
 
 if [ -z "$(pidof $PROCESS)" ]
-	then
-	MAIL=`mktemp`
+  then
+  MAIL=`mktemp`
 
-	echo "$PROCESS not running on $FQDN." > $MAIL
-	echo "Initiating Heartbeat Standby and service restart." >> $MAIL
-	echo >> $MAIL
+  echo "$PROCESS not running on $FQDN." > $MAIL
+  echo "Initiating Heartbeat Standby and service restart." >> $MAIL
+  echo >> $MAIL
 
-	tail -n5 /var/log/mysql/error.log /var/log/mysql.err /var/log/mysql.log >> $MAIL
-	
-	/usr/share/heartbeat/hb_standby all &> /dev/null
-	service mysql restart &> /dev/null
+  tail -n5 /var/log/mysql/error.log /var/log/mysql.err /var/log/mysql.log >> $MAIL
 
-	echo >> $MAIL
-	tail -n10 /var/log/heartbeat-debug >> $MAIL
+  /usr/share/heartbeat/hb_standby all &> /dev/null
+  service mysql restart &> /dev/null
 
-	cat $MAIL | mail -s "$PROCESS not running on $FQDN." $RCPT
-	
-	rm $MAIL
+  echo >> $MAIL
+  tail -n10 /var/log/heartbeat-debug >> $MAIL
+
+  cat $MAIL | mail -s "$PROCESS not running on $FQDN." $RCPT
+
+  rm $MAIL
 fi
