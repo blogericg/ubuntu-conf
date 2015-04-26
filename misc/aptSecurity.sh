0b6c3fd34ffcf200deefcdff30a2fb11323d5bfd
konstruktoid <konstruktoid@users.noreply.github.com>
2015-04-26 21:18:18 +0200

    spaces

--- misc/aptSecurity.sh
+++ misc/aptSecurity.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SRC="/etc/apt/sources.list"
 SSRC="/etc/apt/security.sources.list"
- 
-APT="apt-get -q" 
+
+APT="apt-get -q"
 OPT="-o Dir::Etc::SourceList"
- 
+
 grep -i security $SRC | grep -v "#" > $SSRC
 
 $APT update
@@ -13,4 +13,4 @@ $APT $OPT=$SSRC upgrade
 $APT clean
 $APT autoclean
 $APT autoremove
-update-grub 
+update-grub
