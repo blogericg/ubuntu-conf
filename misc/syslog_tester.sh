892ba23b8f8fe25bd15668fd297700974ba2b062
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-07-31 00:59:01 +0200

    quotes and backticks
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- misc/syslog_tester.sh
+++ misc/syslog_tester.sh
@@ -2,11 +2,11 @@
 facility=(kern user mail daemon auth syslog authpriv)
 severity=(emerg alert crit err warn notice info debug)
 n=10
-host=`hostname -f`
+host=$(hostname -f)
 
 for ((i=1; i<=n; i++))
 do
   PRIORITY0="${facility[RANDOM%${#facility[@]}]}" {,}
   PRIORITY1="${severity[RANDOM%${#severity[@]}]}" {,}
-  logger -p $PRIORITY0.$PRIORITY1 "$host Syslog Test Message $i/$n"
+  logger -p "$PRIORITY0.$PRIORITY1" "$host Syslog Test Message $i/$n"
 done
