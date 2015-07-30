10b04feaa5d1e7e4e889ba8a5de6878ef8628911
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-07-31 00:35:03 +0200

    keep up to date w hardening code
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- net/timesyncd.sh
+++ net/timesyncd.sh
@@ -1,36 +1,42 @@
 function timesyncd {
   echo "Configuring systemd-timesyncd"
 
-  SERVERPOOL="0.ubuntu.pool.ntp.org 1.ubuntu.pool.ntp.org 2.ubuntu.pool.ntp.org 3.ubuntu.pool.ntp.org pool.ntp.org"
+  NTPSERVERPOOL="0.ubuntu.pool.ntp.org 1.ubuntu.pool.ntp.org 2.ubuntu.pool.ntp.org 3.ubuntu.pool.ntp.org pool.ntp.org"
   LATENCY="50"
   SERVERS="4"
   APPLY="NO"
   CONF="/etc/systemd/timesyncd.conf"
-  
   SERVERARRAY=()
   FALLBACKARRAY=()
-  TMPCONF=`mktemp --tmpdir ntpconf.XXXXX`
+  TMPCONF=$(mktemp --tmpdir ntpconf.XXXXX)
 
-  echo "[Time]" > $TMPCONF
+  ID=$(id -u)
+  if [ "x$ID" != "x0" ]; then
+    SUDO="sudo"
+  else
+    SUDO=""
+  fi
 
-  if ! [[ `find /bin/ping -perm -4000` ]];
+  echo "[Time]" > "$TMPCONF"
+
+  if ! [[ $(find /bin/ping -perm -4000) ]];
     then
       PONG="$SUDO ping -c2"
     else
       PONG="ping -c2"
   fi
 
-  for s in `dig +noall +answer +nocomments $SERVERPOOL | awk '{print $5}'`;
+  for s in $(dig +noall +answer +nocomments $NTPSERVERPOOL | awk '{print $5}');
     do
-      if [[  $NUMSERV -ge $SERVERS ]];
+      if [[ $NUMSERV -ge $SERVERS ]];
         then
           break
       fi
-      PINGSERV=`$PONG $s | grep 'rtt min/avg/max/mdev' |awk -F "/" '{printf "%.0f\n",$6}'`
+      PINGSERV=$($PONG "$s" | grep 'rtt min/avg/max/mdev' | awk -F "/" '{printf "%.0f\n",$6}')
       if [[ $PINGSERV -gt "1" && $PINGSERV -lt "$LATENCY" ]];
         then
-          OKSERV=`nslookup $s|grep "name = "|awk '{print $4}'|sed 's/.$//'`
-            if [[ $OKSERV && $NUMSERV -lt $SERVERS && ! (( $(grep "$OKSERV" $TMPCONF) )) ]];
+          OKSERV=$(nslookup "$s"|grep "name = " | awk '{print $4}'|sed 's/.$//')
+            if [[ $OKSERV && $NUMSERV -lt $SERVERS && ! (( $(grep "$OKSERV" "$TMPCONF") )) ]];
               then
                 echo "$OKSERV has latency < $LATENCY"
                 SERVERARRAY+=("$OKSERV")
@@ -39,7 +45,7 @@ function timesyncd {
       fi
   done
 
-  for l in $SERVERPOOL;
+  for l in $NTPSERVERPOOL;
     do
       if [[ $FALLBACKSERV -lt "2" ]];
         then
@@ -50,18 +56,19 @@ function timesyncd {
       fi
     done
 
-  echo "NTP=${SERVERARRAY[@]}" >> $TMPCONF
-  echo "FallbackNTP=${FALLBACKARRAY[@]}" >> $TMPCONF
+  echo "NTP=${SERVERARRAY[@]}" >> "$TMPCONF"
+  echo "FallbackNTP=${FALLBACKARRAY[@]}" >> "$TMPCONF"
 
   if [[ $APPLY = "YES" ]];
     then
       $SUDO bash -c "cat $TMPCONF > $CONF"
-      $SUDO systemctl restart systemd-timesyncd
-      rm $TMPCONF
+      systemctl restart systemd-timesyncd
+      systemctl status systemd-timesyncd
+      rm "$TMPCONF"
     else
       echo "Configuration saved to $TMPCONF."
   fi
-  ((i++))
+
 }
 
 timesyncd
